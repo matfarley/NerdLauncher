41ba13628795a19c5280174a2a448ff8db2c8aed
Matthew Farley <matfarley@gmail.com>
2013-11-21 09:23:21 +1300

    Chapter 23 Big Nerd Ranch Android book

---
+++ src/com/example/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,12 @@
+package com.example.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+	@Override
+	public Fragment createFragment(){
+		return new NerdLauncherFragment();
+	}
+
+}
